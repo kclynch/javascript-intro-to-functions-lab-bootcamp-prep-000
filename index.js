d4fd1e05a480f3b790cf9429eba038ccf300af36
kclynch94@gmail.com <kclynch94@gmail.com>
2018-06-06 02:19:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+ var x=string
+ var y=string.toUpperCase()
+ console.log(y)
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string===string.toLowerCase()) {
+    return "I can't hear you"
+  }
+    else if (string===toUpperCase) 
+  }
+}
+
